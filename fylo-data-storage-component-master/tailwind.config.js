dc92ef3879e763cb02c8c128c977622e0513c9ba
ethanr33 <35584218+ethanr33@users.noreply.github.com>
2020-06-06 11:21:14 -0700

    Added storage component
    Added storage component

---
+++ fylo-data-storage-component-master/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "pale-blue": "hsl(243, 100%, 93%)",
+        "grayish-blue": "hsl(229, 7%, 55%)",
+        "dark-blue": "hsl(228, 56%, 26%)",
+        "very-dark-blue": "hsl(229, 57%, 11%)"
+      },
+      fontFamily: {
+        "display": "Raleway, sans-serif"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
